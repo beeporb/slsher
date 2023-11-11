562f591c76e0e01e650e37cfbdc952190d882a53
beeporb <71353386+beeporb@users.noreply.github.com>
2023-11-11 23:32:10 +0000

    feat: add configuration for pickle to celery

--- server/slsher/services/config.py
+++ server/slsher/services/config.py
@@ -1,8 +1,12 @@
 import celery
 
-from server.slsher.services.config import SLSHER_REDIS_URI
+from slsher.config import SLSHER_REDIS_URI
 
-app = celery.Celery(
-    broker=SLSHER_REDIS_URI + "/0",
-    backend=SLSHER_REDIS_URI + "/1"
-)
+app = celery.Celery(broker=SLSHER_REDIS_URI + "/0",
+                    backend=SLSHER_REDIS_URI + "/1")
+app.conf.event_serializer = "pickle"
+app.conf.task_serializer = "pickle"
+app.conf.result_serializer = "pickle"
+app.conf.accept_content = [
+    "application/json", "application/x-python-serialize"
+]
